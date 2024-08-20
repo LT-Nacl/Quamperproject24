800dbdca8c769e8250f69daf7a9db18ab6ab37d5
LT-Nacl <139832384+LT-Nacl@users.noreply.github.com>
2024-08-20 14:32:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-####                                                        ####
-#### UNFINISHED :: DO NOT RUN WITH > 3 SUPPLIES (or at all) ####
 ####        COMPLETELY UNTESTED POWERSUPPLY CODE            ####
 
 
@@ -7,10 +5,7 @@ import time
 import pyvisa
 import asyncio
 from bleak import BleakClient
-import numpy
-ax = 0.0
-ay = 0.0
-az = 0.0
+import numpy as np
 ###PID WEIGHTS###
 pgain = 1
 igain = 1
@@ -19,8 +14,7 @@ dgain = 1
 # Define the UUIDs of the BLE service and characteristic
 SERVICE_UUID = "12345678-1234-5678-1234-56789abcdef0"
 CHARACTERISTIC_UUID = "12345678-1234-5678-1234-56789abcdef1"
-sensor1 = [0.0,0.0,0.0]
-sensor2 = [0.0,0.0,0.0]
+
 
 
 
@@ -69,7 +63,7 @@ def rotate_vector(vector, adjustx, adjusty, adjustz):
 
 
 #####POWER CONTROL#####
-def VectorWrite(i,j,k,sec): #floats < 1, only
+def VectorWrite(i,j,k,sec,devices): #floats < 1, only
     vector = [i,j,k]
 
     if sec:
@@ -83,14 +77,17 @@ def VectorWrite(i,j,k,sec): #floats < 1, only
             #high v limit just incase, tune if it doesn't work. I can't do that without the supplies
 
 def powersup(voltage, currentlim, device):
-    raw = ('*RST#'
+    if currentlim > 1:
+        currentlim = 1
+    if currentlim < -1:
+        currentlim = -1
+    if currentlim < .01 and currentlim > -.01:
+        currentlim = .01
+    raw = (
     ':SOUR:FUNC VOLT#'
     f':SOUR:VOLT {voltage}#'
     f':SOUR:VOLT:ILIM {currentlim}#'
-    f':TRIG:LOAD "SimpleLoop", 1, 1#' ## prob dont need this. will test
     ':OUTP ON#'
-    ':INIT#'
-    '*WAI#'
     #':OUTP OFF'
     ).format(voltage=voltage, currentlim=currentlim, time=time)
 #   print(raw)
@@ -125,99 +122,103 @@ class PIcontroller: ### look, I know the method/class name isn't updated.
 
         return npv
 #####CONTROL LOOP#####
-
+xcon = PIcontroller() #init for each axis. One of these isn't needed
+ycon = PIcontroller() #as gravity restrains the motion, but I won't know which it is until
+zcon = PIcontroller() #I can test
 
 #########################
 ##### MAIN FUNCTION #####
 async def run(addresses):
     clients = [BleakClient(address) for address in addresses]
-
     try:
         # Connect to all devices
-        await asyncio.gather(*[client.connect() for client in clients])
-        print(f"Connected to devices: {[client.address for client in clients]}")
+        await asyncio.gather(*[i.connect() for i in clients])
 
         # Function to read data from a single client
-        async def read_data(client):
+        async def read_data(client0,client1):
             try:
+                consolebrick()
 
+                rm = pyvisa.ResourceManager() # init Resource Manager
+                print(rm.list_resources())
+                g = 'TCPIP0::192.168.0.21::inst0::INSTR,TCPIP0::192.168.0.22::inst0::INSTR,TCPIP0::192.168.0.23::inst0::INSTR,TCPIP0::192.168.0.24::inst0::INSTR,TCPIP0::192.168.0.25::inst0::INSTR,TCPIP0::192.168.0.26::inst0::INSTR' # list all ports. Only select from the ones that aren't system default ("ASRL")
+                g = g.split(",")
+                print("available devices:")
+                for i in g:
+                    print(i + '\n')
 
+                devices = []
+
+                for i in g: #initialize each of the devices
+                    devices.append(rm.open_resource(i))
+                    print(i + "init!\n")
+                VectorWrite(float(input("setpoint i: ")), float(input("setpoint j: ")), float(input("setpoint k: ")), False, devices)
+
+                ax =1.0
+                ay =1.0
+                az =1.0
                 ##### MAIN LOOP #####
                 while True:
-                    data = await client.read_gatt_char(CHARACTERISTIC_UUID)
-                    print(f"Received data from {client.address}: {data.decode('utf-8')}")
+                    sensor1 = [0,0.0,0.0,0.0]
+                    sensor2 = [0,0.0,0.0,0.0]
+
+                    data = await client0.read_gatt_char(CHARACTERISTIC_UUID)
+                    print(f"Received data from {client0.address}: {data.decode('utf-8')}")
+                    data1 = await client1.read_gatt_char(CHARACTERISTIC_UUID)
+                    print(f"Received data from {client1.address}: {data1.decode('utf-8')}")
+
                     data_str = data.decode('utf-8')
                     data_list = data_str.split(',')
+                    data1_str = data1.decode('utf-8')
+                    data1_list = data1_str.split(',')
+
+                    sensor1 = data_list
+                    sensor2 = data1_list
+
+                    print(sensor1)
+                    print(sensor2)
+
                     n = 0
-                    while n < len(data_list):
-                        if n > 0:
-                            data_list[n] = float(data_list[n])
-                        else:
-                            data_list[n] = int(data_list[n]) # to use inbuilt index, i dont want to work with addr
-                        n += 1
-                    if data_list[0] == 1:
-                        sensor1 = data_list
-                    else:
-                        sensor2 = data_list
-
-                    adjustx = xcon.PI(sensor1[0],sensor2[0])
-                    adjusty = ycon.PI(sensor1[0],sensor2[0])
-                    adjustz = zcon.PI(sensor1[0],sensor2[0])
-
+                    while n < len(sensor1):
+                        sensor1[n] = float(sensor1[n])
+                        sensor2[n] = float(sensor2[n])
+                        n+=1
+                    adjustx = xcon.PI(sensor1[1],sensor2[1])
+                    adjusty = ycon.PI(sensor1[2],sensor2[2])
+                    adjustz = zcon.PI(sensor1[3],sensor2[3])
                     newv = rotate_vector([ax, ay, az], adjustx, adjusty, adjustz)
                     ax = newv[0]
                     ay = newv[1]
                     az = newv[2]
-
-                    VectorWrite(ax,ay,az,True) ### write updated values to second set of powersupplies
+                    print('hi')
+                    VectorWrite(ax,ay,az,True,devices) ### write updated values to second set of powersupplies
 
                     ### vec_update will be added to list of currents abd ->SHOULD<- directly respond.
                     ### in my dreams ...
-                    await asyncio.sleep(.2)  # Read every .2 seconds, should probably decrease for more accurate pid
+                    await asyncio.sleep(.1)  # Read every .2 seconds, should probably decrease for more accurate pid
                 ##### MAIN LOOP #####
 
 
             except Exception as e:
-                print(f"Error with {client.address}: {e}")
+                print(f"Error: {e}")
 
         # Start reading data from each BLE device
-        await asyncio.gather(*[read_data(client) for client in clients])
+        await asyncio.gather(*[read_data(clients[0],clients[1])])
 
     except Exception as e:
         print(f"Failed to connect or read data: {e}")
 
     finally:
         # Disconnect from all devices
-        await asyncio.gather(*[client.disconnect() for client in clients])
-        print("Disconnected from all devices")
+        print("done!")
 #####MAIN FUNCTION#####
 #######################
 
 
 #####CALL#####
 def main():
-    xcon = PIcontroller() #init for each axis. One of these isn't needed
-    ycon = PIcontroller() #as gravity restrains the motion, but I won't know which it is until
-    zcon = PIcontroller() #I can test
-    consolebrick()
-
-    rm = pyvisa.ResourceManager() # init Resource Manager
-    g = rm.list_resources() # list all ports. Only select from the ones that aren't system default ("ASRL")
-
-    print("available devices:")
-    for i in g:
-      print(i + '\n')
-
-    addrs = [] #ports for initializing devices
-
-    print('pick n \n') # pick how many power supplies you want
-    numadd = input('n:\n') # 1 sphere  -> 3, # 2 sphere -> 6
-    for i in range(int(numadd)):
-        addrs.append(g[i])
-
     ## init vectors
-    VectorWrite(float(input("setpoint i: ")), float(input("setpoint j: ")), float(input("setpoint k: ")), False)
-    VectorWrite(ax, ay, az, True)
+
 
     #####BLUETOOTH LE#####
     addresses = ['b9:e9:78:3f:10:da','9e:9d:53:fa:dc:16']
